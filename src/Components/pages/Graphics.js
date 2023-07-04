a9c3b9c40e8caec369c4f145343d13a1b49c5911
Cristiano Bento <crisdb2008@gmail.com>
2023-07-04 15:10:41 -0300

    nocors

--- src/Components/pages/Graphics.js
+++ src/Components/pages/Graphics.js
@@ -25,7 +25,7 @@ async function getData(){
     const options = { 
         'Access-Control-Allow-Origin':'*',
         method: 'GET',	
-        mode: 'cors',
+        mode: 'no-cors',
         cache: 'default',
         'Content-Type': 'application/json'}
      fetch(`https://test-no-vercel.vercel.app/temps`,options)
