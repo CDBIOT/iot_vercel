c7d7ac6c60a665f6f62d20600f10bbc3e20d195e
CDBIOT <crisdb2008@gmail.com>
2023-10-12 11:37:52 -0300

    tem´p

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -69,7 +69,7 @@ return (
     <>
     <h1>Schedule</h1>
     <table>
-    <tr><td  >Temperatura: {temp[10]}  Local: { local } Data: { dia } / { mes } / { ano }</td></tr>
+    <tr><td  >Temperatura: {temps[temp]}  Local: { local } Data: { dia } / { mes } / { ano }</td></tr>
     </table>
     {/* <div className = {styles.temp_show}>
         <table>
