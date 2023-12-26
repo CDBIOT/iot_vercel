f61d8fab5bfdd5647b871d183a4cf68ba7cfd863
CDBIOT <cristiab05@yahoo.com.br>
2023-12-26 18:25:42 -0300

    on Lmap

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -164,10 +164,10 @@ return (
         <span id="rele">N/D</span>
     </tr>
     <tr><td>
-	    <input type="button" id="lamp"   value="onLamp"    onClick="onLamp()"/>
-        <input type="button" id="aqua_lamp"   value="onAquaLamp"    onClick="setAqua_lamp()"/>
-	    <input type="button" id="pump"  value="setPump" onClick="setPump()"/>
-	    <input type="button" id="vent" value="setVent"    onClick="setVent()"/>
+	    <input type="button" id="lamp"   value="onLamp"    onClick= {onLamp()}/>
+        <input type="button" id="aqua_lamp"   value="onAquaLamp"    onClick={setAqua_lamp()}/>
+	    <input type="button" id="pump"  value="setPump" onClick={setPump()}/>
+	    <input type="button" id="vent" value="setVent"    onClick={setVent()}/>
     </td></tr>
     </table>
         </form>
