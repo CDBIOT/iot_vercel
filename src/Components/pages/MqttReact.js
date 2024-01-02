a9f7d41a0cbf18cdfbbbb6e070a31448b2356f92
CDBIOT <cristiab05@yahoo.com.br>
2024-01-02 18:06:16 -0300

    1.0.1

--- src/Components/pages/MqttReact.js
+++ src/Components/pages/MqttReact.js
@@ -6,22 +6,22 @@ import {Connector} from "mqtt-react-hooks"
 
 
 function MqttReact(){
-  // const topic = 'Sala';
-  // const path = '/mqtt';
-  // const payload = 'temp';
-  // const host = 'broker.mqtt-dashboard.com'
-  // const port = '1883'
-  // const clientId = 'mqttjs_'+ Math.random().toString(16).slice(3)
-  // const connectUrl = 'mqtt:'+ {host}+':'+{port}
-  // const options = {
+   const topic = 'Sala';
+   const path = '/mqtt';
+   const payload = 'temp';
+   const host = 'broker.mqtt-dashboard.com'
+   const port = '1883'
+   const clientId = 'mqttjs_'+ Math.random().toString(16).slice(3)
+   const connectUrl = 'mqtt:'+ {host}+':'+{port}
+   const options = {
     
-  //      clientId,
-  //      clean: true,
-  //      connectTimeout: 4000,
-  //      username: 'emqx',
-  //      password: 'public',
-  //      reconnectPeriod: 3000,
-  //   }
+        clientId,
+        clean: true,
+        connectTimeout: 4000,
+        username: 'emqx',
+        password: 'public',
+        reconnectPeriod: 3000,
+     }
 
   //const[client, setClient] = useState(null)
   const[connectionStatus, setConnectionStatus] =useState(false)
@@ -30,29 +30,29 @@ function MqttReact(){
 
 useEffect(() =>{
   
-// const client = (mqtt.connect(connectUrl,options))
-//    try{
-//  client.on('connect', () => {
-//    setConnectionStatus(true)
-//    console.log('Connected')
-//    setConnectionStatus(true)
-//  })}catch (error){console.log('mqtt.connect error',error)}
+const client = (mqtt.connect(connectUrl,options))
+   try{
+ client.on('connect', () => {
+   setConnectionStatus(true)
+   console.log('Connected')
+   setConnectionStatus(true)
+ })}catch (error){console.log('mqtt.connect error',error)}
 
-// client.subscribe('Sala', () => {
-//   console.log("Subscribe to topic:", +topic)
+client.subscribe('Sala', () => {
+  console.log("Subscribe to topic:", +topic)
 
-// client.on('message', (topic, payload) => {
-// setMessages(payload.toString())
-//      //temp = payload
-//      //local= topic
-//    console.log('Received Message:', + payload.toString())
-//    // res.status(200).json({m})
-//  })
-//}) 
+client.on('message', (topic, payload) => {
+setMessages(payload.toString())
+     //temp = payload
+     //local= topic
+   console.log('Received Message:', + payload.toString())
+   // res.status(200).json({m})
+ })
+}) 
 
 },[]);
-//console.log("Connections: " +connectionStatus)
-//console.log("Messages: " +messages)
+console.log("Connections: " +connectionStatus)
+console.log("Messages: " +messages)
 
 // setInterval(() => {
 // client.on('message', (topic, payload) => {
@@ -64,20 +64,20 @@ useEffect(() =>{
   
 // }, 1000);
 
-// const topic2 = 'Quarto'
-// client.on('connect', () => {
-//   console.log('Connected')
-//   client.subscribe([topic2], () => {
-//     console.log(`Subscribe to topic '${topic2}'`)
+ const topic2 = 'Quarto'
+ client.on('connect', () => {
+   console.log('Connected')
+  client.subscribe([topic2], () => {
+     console.log(`Subscribe to topic '${topic2}'`)
 
-// client.on('message', (topic2, payload) => {
-//       temp = payload
-//       local= topic2
-     // console.log('Received Message:', topic, payload.toString())
+ client.on('message', (topic2, payload) => {
+       temp = payload
+             local= topic2
+      console.log('Received Message:', topic, payload.toString())
       //res.status(200).json({m})
-//     })
-//   })
-// })
+     })
+   })
+ })
 
  // client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: true }, (error) => {
 //  if (error) {
@@ -86,13 +86,13 @@ useEffect(() =>{
  // })
  return(
   <div>
-   {/* <Connector brokerUrl='broker.mqtt-dashboard.com:1883'/>
-    <label >Status: {connectionStatus}</label>*/}
+    <Connector brokerUrl='broker.mqtt-dashboard.com:1883'/>
+    <label >Status: {connectionStatus}</label>
 				<table className = {styles.table}>   
 					<tr><th className = {styles.thead} colSpan={2}>TEMPERATURA DA SALA </th></tr>
 					<tr>
-						{/* <td>Local: </td><td colSpan={4}><h2>{topic}</h2></td>
-						<td>Temp: </td><td colSpan={4}><h2>{payload}</h2></td> */}
+						 <td>Local: </td><td colSpan={4}><h2>{topic}</h2></td>
+						<td>Temp: </td><td colSpan={4}><h2>{payload}</h2></td> 
 					</tr>
         </table> 
   </div>
