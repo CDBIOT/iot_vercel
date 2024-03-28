252c1e9ca58e9073c3b495344f4a6d4fe4eb0d0e
CDBIOT <cristiab05@yahoo.com.br>
2024-03-28 18:49:31 -0300

    button

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -222,7 +222,7 @@ return (
             <span id="rele">N/D</span>
         </tr>
         <tr><td>
-	        <input type="button" id="onlamp"   value="onLamp"    onClick= {onLamp}/>
+	        <input className={styles.button} type="button" id="onlamp"   value="onLamp"    onClick= {onLamp}/>
             <input type="button" id="offlamp"   value="offLamp"    onClick={offLamp}/>
 	        <input type="button" id="pump"  value="setPump" onClick={onPump}/>
 	        <input type="button" id="vent" value="setVent"    onClick={offPump}/>
