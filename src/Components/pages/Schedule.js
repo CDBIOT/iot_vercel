034959d4bfec9612eafdd107408c76d42c7962e3
CDBIOT <cristiab05@yahoo.com.br>
2025-01-21 15:30:09 -0300

    updae

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -203,6 +203,17 @@ await Axios.get (('https://test-no-vercel.vercel.app/subscriber'),options)
 
 }
 
+const sendUpdate = useCallback(() => {
+    websocket.current?.send(
+      JSON.stringify({
+        type: "message",
+        LED: led,
+      })
+    );
+  }, []);
+
+  const toggleLed = useCallback(() => sendUpdate({ led: !LED }), [LED, sendUpdate]);
+
 return (
     <>
     <h1>Schedule</h1>
