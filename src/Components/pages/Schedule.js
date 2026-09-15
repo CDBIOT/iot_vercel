6026f4dc14b952c5e13c7871f71d6585298766a5
CDBIOT <cristiab05@yahoo.com.br>
2026-09-15 16:12:23 -0300

    h

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -81,8 +81,8 @@ await Axios.get (('https://test-no-vercel.vercel.app/mqtt'),options)
 }
 
 
-//const connectUrl = 'wss://5d3be4977c10482289edf71c15f420fe.s1.eu.hivemq.cloud:8884/mqtt';
-const connectUrl = 'wss://broker.mqtt-dashboard.com:8084/mqtt'
+const connectUrl = 'wss://5d3be4977c10482289edf71c15f420fe.s1.eu.hivemq.cloud:8884/mqtt';
+//const connectUrl = 'wss://broker.mqtt-dashboard.com:8084/mqtt'
    
 const options = {
   
